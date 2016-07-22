2e517b8a59ea0eea22c19cd94598465a1f91e93c
fabry <fabry_00@hotmail.com>
2016-07-22 18:56:47 +0200

    starting datase

--- start_services.sh
+++ start_services.sh
@@ -15,7 +15,7 @@ sleep 5s
 echo "*************************************************************"
 echo "******************** Task2 Service *************************"
 sleep 1s
-java -jar Task2/targetTask1-2.0-SNAPSHOT-launcher.jar server Task2/task2service.yml  50 &
+java -jar Task2/targetTask-1.0-SNAPSHOT-launcher.jar server Task2/task2service.yml  50 &
 PID=$!
 echo "PID PrOCESS: $PID"
 PIDs="$PIDs $PID "
